611c9deb5dc6a386742dc16ae5f95a76ea8f458d
Paraskevas <vaslysalex@hotmail.gr>
2023-11-23 16:51:48 +0200

    backend starting user

--- BackEnd/app/Http/Controllers/AuthController.php
+++ BackEnd/app/Http/Controllers/AuthController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use App\Traits\HttpResponses;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -12,5 +15,27 @@ class AuthController extends Controller
     public function login(Request $request){
         return "login here";
     }
+
+    public function register(StoreUserRequest $request){
+        $request->validated($request->all());
+
+            $user=User::create([
+                'name'=> $request->name,
+                'email'=> $request->email,
+                'password'=>Hash::make($request->password),
+            ]);
+
+        return $this->success([
+            'user'=>$user,
+            'token'=>$user->createToken('api token of '.$user->name)->plainTextToken
+        ]);
+
+    }
+
+
+    public function logout(Request $request){
+        return "logout here";
+    }
+
 
 }
